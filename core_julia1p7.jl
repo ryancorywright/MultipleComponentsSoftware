28f58caf554da67e9e04cf9670bf1f4f591c871e
Ryan Cory-Wright <34351783+ryancorywright@users.noreply.github.com>
2023-10-30 13:09:03 +0000

    Add files via upload

--- core_julia1p7.jl
+++ core_julia1p7.jl
@@ -1,2 +1,4 @@
 using JuMP, Random, LinearAlgebra, JLD, DataFrames, Test, Suppressor, DelimitedFiles, CSV, StatsBase, MosekTools, Compat, Arpack
 using MathOptInterface, Gurobi, MosekTools
+
+global const GRB_ENV = Gurobi.Env()
